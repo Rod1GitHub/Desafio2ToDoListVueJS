9742dd2ef1b365941be916ac1dbfafb82e90d65b
Rod <rod@Rods-MacBook-Pro.local>
2022-01-28 22:07:34 -0300

    usando nombres con claves para encontrar las inter-conecciones

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-
   <div id="app-todolist">
-    <a href="https://github.com/Rod1GitHub/Desafio2ToDoListVueJS" target="_blank">Link to git</a>
+    <a
+      href="https://github.com/Rod1GitHub/Desafio2ToDoListVueJS"
+      target="_blank"
+      >Link to git</a
+    >
     <h1>Tareas para esta semana</h1>
     <div>
       <h2>Ingresar tarea:</h2>
-      <input id="task" type="text" v-model="task"/>
-      <button @click="addTaskMethod">Agregar a lista</button>
+      <input id="idInputTask" type="text" v-model="taskSingularInput003" />
+      <button @click="captureInputAndAddToEndOfArrayMethod003">
+        Capturar y agregar al Array
+      </button>
     </div>
     <div>
       <h2>Lista de tareas por hacer:</h2>
       <ul>
-        <li v-for="(task, i) in tasks" :key="i">
-          {{ getTaskMethod(i+1, task ) }}
+        <li
+          v-for="(taskArgument003, index003) in tasksPluralArray003"
+          :key="index003"
+        >
+          {{ printFromArrayToHtmlMethod003(index003 + 1, taskArgument003) }}
         </li>
       </ul>
     </div>
@@ -24,17 +32,17 @@ export default {
   name: "App",
   data() {
     return {
-      task: "",
-      tasks: [],
+      taskSingularInput003: "",
+      tasksPluralArray003: [],
     };
   },
   methods: {
-    getTaskMethod(index, task) {
-      return `${index}: ${task}`;
+    captureInputAndAddToEndOfArrayMethod003() {
+      let { taskSingularInput003 } = this; // Esto captura el input
+      this.tasksPluralArray003.push(taskSingularInput003); // push() lo envía al final del array
     },
-    addTaskMethod() {
-      let { task } = this;
-      this.tasks.push(task);
+    printFromArrayToHtmlMethod003(indexParam003, taskParam003) {
+      return `${indexParam003}: ${taskParam003}`;
     },
   },
 };
